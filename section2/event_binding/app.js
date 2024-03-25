dee2e1100c73aa3ba8e3ede21b436a0f87d99624
jinunu <mybark7@gmail.com>
2024-03-25 18:26:04 +0900

    vue 섹션 2 : 이벤트 수식어 알아보기
    * 브라우저 기본값 버튼이 있는 양식에서 버튼 클릭 시 앱을 제공하는 http 요청을 보내게 되어 새로고침이 일어난다.
    => 유효성 체크 후 전송 되어야 하기 때문에 기본값 방지 필요
    
    event 객체의 preventDefault()를 대체하는 vue 수식이 .prevent 사용.
    
    v-on 이벤트는 여러개 사용 가능

--- section2/event_binding/app.js
+++ section2/event_binding/app.js
@@ -1,25 +1,34 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name : ''
-    };
-  },
-  methods: {
-    add(num) {
-       this.counter += num;
+    data() {
+        return {
+            counter: 0,
+            name: '',
+            confirmedName: ''
+        };
     },
-    reduce(num){
-       this.counter -= num;
-    },
-    // setName(event)  {
-    //   this.name = event.target.value;
-    // }
-    setName(event, addWord)  {
-      this.name = event.target.value + ' ' + addWord;
-    }
+    methods: {
+        confirmInput() {
+            this.confirmedName = this.name;
+        }
+        ,
+        submitForm(event) {
+            // event.preventDefault();
+            alert('Submitted!');
+        },
+        add(num) {
+            this.counter += num;
+        },
+        reduce(num) {
+            this.counter -= num;
+        },
+        // setName(event)  {
+        //   this.name = event.target.value;
+        // }
+        setName(event, addWord) {
+            this.name = event.target.value + ' ' + addWord;
+        }
 
-  }
+    }
 });
 
 app.mount('#events');
