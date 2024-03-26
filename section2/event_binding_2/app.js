3a10bcdef4e0e7e03245c25e39df00a556878b2a
jinunu <mybark7@gmail.com>
2024-03-26 13:53:36 +0900

    vue 섹션 2 데이터 바인딩에 사용되는 메서드 작동 원리, 연산(computed)프로퍼티 소개
    - Vue는 리랜더링시 모든 메소드를 호출 한다. 주의 하여 사용해야함.
    
    - computed 호출 및 실행 방법
     - 연산 프로퍼티 이름은 data 프로퍼티처럼 정해야 한다.
       why? 메서드처럼 사용하지 않고 호출도 하지 않기 때문. 메서드지만 변수처럼 사용

--- section2/event_binding_2/app.js
+++ section2/event_binding_2/app.js
@@ -5,8 +5,24 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    fullName() {
+      console.log('재실행');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + '크하하하';
+    }
+  },
   methods: {
-    setName(event, lastName) {
+    outputFullname() {
+      console.log('재실행');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + '크하하하';
+    },
+    setName(event) {법
       this.name = event.target.value;
     },
     add(num) {
